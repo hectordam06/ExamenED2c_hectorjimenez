89811d30dca9b18bdf5de97f9d35b6ea0a50cfe7
hectordam06 <dam06.2023@gmail.com>
2023-03-31 09:47:54 +0200

    proyecto documentado

--- src/examen/Hora.java
+++ src/examen/Hora.java
@@ -1,5 +1,36 @@
 package examen;
-
+/**
+ * Se trata de un programa que valida la hora dependiendo de si los parametros introducidos por consola son correctos o no
+ * 
+ * 
+ * @author Héctor Jiménez
+ *@version 1.0
+ */
 public class Hora {
+	public static boolean horaValida;
+/**
+ * 
+ * metodo que valida la hora.
+ * 
+ * @param segundos numero de segundos introduciodos
+ * @param minutos numero de minutos introduciodos
+ * @param horas numero de horas introducidas
+ * @return devuelve un booleano que dice si la hora es valida o no 
+ */
+	public static boolean validarHora(int segundos, int minutos, int horas) {
+		horaValida = true;
+		if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
+			if (horas > 23) {
+				horaValida = false;
+			} else if (minutos > 59) {
+				horaValida = false;
+			} else if (segundos > 59) {
+				horaValida = false;
+			}
+		} else {
+			horaValida = false;
+		}
+		return horaValida;
+	}
 
 }
