0d209a5863398114fa0830364d5dbc964f59c78a
hectordam06 <dam06.2023@gmail.com>
2023-03-31 09:38:00 +0200

    proyecto sin documentar

---
+++ src/examen/HoraTest.java
@@ -0,0 +1,30 @@
+package examen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HoraTest {
+
+	@Test
+	void testprueba1() {
+		testprueba=horaValida.validarHora
+		assert false horaValida.validarHora((-1,10,10));
+	}
+
+	@Test
+	void testprueba2() {
+		assert false horaValida.validarHora((-1,10,10));
+	}
+	
+	@Test
+	void testprueba3() {
+		assert false horaValida.validarHora((-1,10,10));
+	}
+
+	@Test
+	void testpruba4() {
+		assert false horaValida.validarHora((-1,10,10));
+	}
+
+}
